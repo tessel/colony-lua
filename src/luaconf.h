a600a6cb83ee4d62565d456c6c969c6ed526e552
Tim Cameron Ryan <id@timryan.org>
2014-07-25 14:38:13 -0700

    Adds Node-like double modulus.

--- src/luaconf.h
+++ src/luaconf.h
@@ -534,7 +534,7 @@
 #define luai_numsub(a,b)	((a)-(b))
 #define luai_nummul(a,b)	((a)*(b))
 #define luai_numdiv(a,b)	((a)/(b))
-#define luai_nummod(a,b)	((a) - floor((a)/(b))*(b))
+#define luai_nummod(a,b)	((a < 0) ? -((-a) - floor((-a)/(b))*(b)) : ((a) - floor((a)/(b))*(b)))
 #define luai_numpow(a,b)	(pow(a,b))
 #define luai_numunm(a)		(-(a))
 #define luai_numeq(a,b)		((a)==(b))
